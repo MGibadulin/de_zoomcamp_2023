af22eaf2cdce6403d7872809df2df02f8526d2aa
MGibadulin <92296162+MGibadulin@users.noreply.github.com>
2023-01-27 12:36:40 +0500

    Update etl_gcs_to_bq.py

--- week_2_workflow_orchestration/flows/02_gcp/etl_gcs_to_bq.py
+++ week_2_workflow_orchestration/flows/02_gcp/etl_gcs_to_bq.py
@@ -8,6 +8,7 @@ from prefect_gcp import GcpCredentials
 @task(retries=3)
 def extract_from_gcs(color: str, year: int, month: int) -> Path:
     """Download trip data from GCS"""
+    
     gcs_path = f"data/{color}/{color}_tripdata_{year}-{month:02}.parquet"
     gcs_block = GcsBucket.load("zoom-gcs")
     gcs_block.get_directory(from_path=gcs_path, local_path=f"../data/")
@@ -17,6 +18,7 @@ def extract_from_gcs(color: str, year: int, month: int) -> Path:
 @task()
 def transform(path: Path) -> pd.DataFrame:
     """Data cleaning example"""
+    
     df = pd.read_parquet(path)
     print(f"pre: missing passenger count: {df['passenger_count'].isna().sum()}")
     df["passenger_count"].fillna(0, inplace=True)
@@ -32,7 +34,7 @@ def write_bq(df: pd.DataFrame) -> None:
 
     df.to_gbq(
         destination_table="dezoomcamp.rides",
-        project_id="prefect-sbx-community-eng",
+        project_id="prefect-sbx-community-eng", # fix it 
         credentials=gcp_credentials_block.get_credentials_from_service_account(),
         chunksize=500_000,
         if_exists="append",
@@ -42,6 +44,7 @@ def write_bq(df: pd.DataFrame) -> None:
 @flow()
 def etl_gcs_to_bq():
     """Main ETL flow to load data into Big Query"""
+    
     color = "yellow"
     year = 2021
     month = 1
